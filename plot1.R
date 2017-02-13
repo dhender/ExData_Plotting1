d73e26383fcfabbc773ba9a07f144a687d9b3051
dhender <donald.hender@gmail.com>
2017-02-12 23:01:42 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,58 @@
+## Project1 - Exploratory Data Analysis
+##
+## Description: 
+## ------------
+## This project/assignment utilizes dataset (Electric power consumption) 
+## from the UC Irvine Machine Learning Repository. A text file was 
+## (household_power_consumption.txt) is extracted into a folder called 'data' 
+## which was created in the working directory.
+##
+## Goal:
+## -----
+## To examine how household energy usage varies over a 2-day period in Feb,2007
+## via plotting of data from household_power_consumption.txt
+##
+## Requirements for each plot:
+## ---------------------------
+## 1. Construct the plot and save it to a PNG file with a width of 480 pixels 
+## and a height of 480 pixels.
+## 2. Adopt the following naming conventions for each of the plots: plot1.png,
+## plot2.png, plot3.png, plot4.png
+## 3. Create a seperate R script file (i.e. plot1.R,plot2.R,etc.) that constructs
+## the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot.
+## The script file includes code for reading the data so that the plot can be 
+## reproduced. The script will also contain code that creates the .png file.
+## 4. Add both the .png file and R-code to the top-level folder of the git repo
+## (no need for seperate sub-folders). 
+## 5. Push Git repository to GitHub so that the GitHub version is up to date. 
+## There should be four .png files and four R script/code files, total of 8 files
+## in top level of repo.
+#
+# Read data from household_power_consumption.txt into R 
+# Note: grep attribute is utilized to import data from 1/2/2007 till 2/2/2007
+# rather than reading in entire dataset
+#
+rm(list = ls())
+#
+dataFile <- "./data/household_power_consumption.txt"
+txtfile <- read.table(text = grep("^[1,2]/2/2007",readLines(dataFile),value=TRUE),col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep=";", quote= "", strip.white=TRUE, na.strings= "?")
+# Convert the date variable to Date class
+txtfile$Date <- as.Date(txtfile$Date, format = '%d/%m/%Y')
+txtfile$DateTime <- as.POSIXct(paste(txtfile$Date, txtfile$Time))
+# --------------------------------------------------------
+# Alternatively if memory size is not a conern then read in entire file and create 
+#txtfile <- read.table(dataFile,nrows = 2075259, sep=";", header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+# Convert the date variable to Date class
+#txtfile$Date <- as.Date(txtfile$Date, format = '%d/%m/%Y')
+#txtfile$DateTime <- as.POSIXct(paste(txtfile$Date, txtfile$Time))
+# Subset the data
+#txtfile <- subset(txtfile, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+# --------------------------------------------------------
+#
+# Open device
+png(filename = './plot1.png', width = 480, height = 480, units='px')
+# Plot data
+#with(txtfile, hist(Global_active_power, xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power', col = 'red'))
+hist(txtfile$Global_active_power, xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power', col = 'red')
+# Close device
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,54 @@
+## Project1 - Exploratory Data Analysis
+##
+## Description: 
+## ------------
+## This project/assignment utilizes dataset (Electric power consumption) 
+## from the UC Irvine Machine Learning Repository. A text file was 
+## (household_power_consumption.txt) is extracted into a folder called 'data' 
+## which was created in the working directory.
+##
+## Goal:
+## -----
+## To examine how household energy usage varies over a 2-day period in Feb,2007
+## via plotting of data from household_power_consumption.txt
+##
+## Requirements for each plot:
+## ---------------------------
+## 1. Construct the plot and save it to a PNG file with a width of 480 pixels 
+## and a height of 480 pixels.
+## 2. Adopt the following naming conventions for each of the plots: plot1.png,
+## plot2.png, plot3.png, plot4.png
+## 3. Create a seperate R script file (i.e. plot1.R,plot2.R,etc.) that constructs
+## the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot.
+## The script file includes code for reading the data so that the plot can be 
+## reproduced. The script will also contain code that creates the .png file.
+## 4. Add both the .png file and R-code to the top-level folder of the git repo
+## (no need for seperate sub-folders). 
+## 5. Push Git repository to GitHub so that the GitHub version is up to date. 
+## There should be four .png files and four R script/code files, total of 8 files
+## in top level of repo.
+#
+# Read data from household_power_consumption.txt into R 
+# Note: grep attribute is utilized to import data from 1/2/2007 till 2/2/2007
+# rather than reading in entire dataset
+#
+rm(list = ls())
+#
+dataFile <- "./data/household_power_consumption.txt"
+txtfile <- read.table(text = grep("^[1,2]/2/2007",readLines(dataFile),value=TRUE),col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep=";", quote= "", strip.white=TRUE, na.strings= "?")
+# Convert the date variable to Date class
+txtfile$Date <- as.Date(txtfile$Date, format = '%d/%m/%Y')
+txtfile$DateTime <- as.POSIXct(paste(txtfile$Date, txtfile$Time))
+# Open device
+png(filename = './plot4.png', width = 480, height = 480, units='px')
+# Plot data
+par(mfrow = c(2, 2))
+plot(txtfile$DateTime, txtfile$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(txtfile$DateTime, txtfile$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(txtfile$DateTime, txtfile$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(txtfile$DateTime, txtfile$Sub_metering_2, col = 'red')
+lines(txtfile$DateTime, txtfile$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(txtfile$DateTime, txtfile$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+# close device
+dev.off()
